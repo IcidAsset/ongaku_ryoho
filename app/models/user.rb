70b663142a9c5a26c46d950608cc3ae06c11f574
Steven Vandevelde <icid.asset@gmail.com>
2012-03-17 19:59:24 +0100

    New user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,14 @@ class User
 
   authenticates_with_sorcery!
 
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+  validates_format_of :email,
+    :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
+    :on => :create,
+    :messsage => "is invalid"
+
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+
 end
