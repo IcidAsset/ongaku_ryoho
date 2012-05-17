1e3c0b96d51d8cb068190c2c63ec7da7f503b799
Steven Vandevelde <icid.asset@gmail.com>
2012-05-17 22:30:15 +0200

    Update source model and add unit tests

--- app/models/source.rb
+++ app/models/source.rb
@@ -3,7 +3,7 @@ class Source
   include Mongoid::Timestamps
   
   field :activated,   type: Boolean,   default: false
-  field :status,      type: String,    default: "unprocessed",   required: true
+  field :status,      type: String,    default: "unprocessed"
   
   embeds_many :tracks
   embedded_in :user
--- test/unit/source_test.rb
+++ test/unit/source_test.rb
@@ -1,4 +1,16 @@
 require 'test_helper'
 
-class SourceTest < ActiveSupport::TestCase
+describe Source do
+  subject { Source }
+  
+  it { must be_document }
+  it { must be_timestamped }
+  
+  it { must have_field(:activated).of_type(Boolean) }
+  it { must have_field(:activated).with_default_value(false) }
+  it { must have_field(:status).of_type(String) }
+  it { must have_field(:status).with_default_value("unprocessed") }
+  
+  it { must embed_many(:tracks) }
+  it { must embedded_in(:user) }
 end
