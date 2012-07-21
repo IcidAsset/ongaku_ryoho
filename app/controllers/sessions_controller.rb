c5c9971d5cc03cea7498dae15f5db6be1647cf38
Steven Vandevelde <icid.asset@gmail.com>
2012-07-21 11:15:51 +0200

    Clean up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    user = login(params[:email], params[:password], params[:remember_me])
+    user = login(
+      params[:email],
+      params[:password],
+      params[:remember_me]
+    )
+
     if user
       redirect_back_or_to root_url, :notice => "Logged in!"
     else
