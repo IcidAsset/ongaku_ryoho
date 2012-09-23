7a28f60ac2c18d5241ba5b2b0c1ddaf3633df06a
Steven Vandevelde <icid.asset@gmail.com>
2012-09-23 18:30:54 +0200

    Add new pages design and replace pt_sans with source_sans_pro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,34 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_pages
 
   def not_authenticated
     redirect_to sign_in_url, :alert => "First login to access this page."
   end
+
+private
+
+  def set_pages
+    @pages = pages
+    @page = @pages.find { |p| p[:url] == request.path }
+  end
+
+  def pages
+    if logged_in?
+      [
+        { title: "Application", url: "/", icon: "&#xe03a;" },
+        { title: "Settings", url: "/settings", icon: "&#x0042;" },
+        { title: "Tools", url: "/tools", icon: "&#x0043;" },
+        { title: "Account", url: "/account", icon: "&#x002b;" },
+        { title: "Sign out", url: "/sign-out", icon: "&#x0024;" }
+      ]
+    else
+      [
+        { title: "About", url: "/about", icon: "&#x0060;" },
+        { title: "Sign in", url: "/sign-in", icon: "&#x002b;" },
+        { title: "Sign up", url: "/sign-up", icon: "&#x002d;" }
+      ]
+    end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,7 @@
 module ApplicationHelper
-  def pages
-    if logged_in?
-      [
-        { title: "Application", url: "/", icon: "&#xe03a;" },
-        { title: "Settings", url: "/settings", icon: "&#x0042;" },
-        { title: "Tools", url: "/tools", icon: "&#x0043;" },
-        { title: "Account", url: "/account", icon: "&#x002b;" },
-        { title: "Sign out", url: "/sign-out", icon: "&#x0024;" }
-      ]
-    else
-      [
-        { title: "About", url: "/about", icon: "&#x0060;" },
-        { title: "Sign in", url: "/sign-in", icon: "&#x002b;" },
-        { title: "Sign up", url: "/sign-up", icon: "&#x002d;" }
-      ]
-    end
+
+  def body_class
+    @page[:title].parameterize
   end
+
 end
