447a8c518c7e0eefb2cc61968c90059d094683f7
Steven Vandevelde <icid.asset@gmail.com>
2013-07-21 16:50:53 +0200

    Deadlock fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ OngakuRyoho::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Enable threaded mode
+  config.threadsafe!
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
