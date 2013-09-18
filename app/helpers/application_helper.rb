82fa5cf9f390ebde49041ca70f20f52b092acb44
Steven Vandevelde <icid.asset@gmail.com>
2013-09-19 00:24:31 +0200

    Theme selection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,31 @@ module ApplicationHelper
     @page[:title].parameterize
   end
 
+  def application_css_filename
+    default = "blue-silver"
+
+    if current_user
+      theme = current_user.settings["theme"] || default
+    else
+      theme = default
+    end
+
+    "application_#{theme}"
+  end
+
+  def theme_names
+    %w(
+      aqua
+      blue-silver
+      earth
+      evening
+      forest
+      grass
+      pink-lad
+      purple-rain
+      red-stone
+      swamp
+    )
+  end
+
 end
